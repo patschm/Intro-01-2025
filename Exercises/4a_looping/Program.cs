f627d14b7cdb6432f2e64f4df54f1cffd25acdfd
unknown <pschmidt@pasit.nl>
2025-01-21 11:52:17 +0100

    day2

---
+++ Exercises/4a_looping/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _4a_looping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // int teller = 0;
+
+            // De for-loop gebruik je als je weet hoe vaak je iets wilt herhalen.
+            for (int teller = 1; teller <= 10; teller++)
+            {
+                if (teller == 9)
+                {
+                    // Stopt de hele loop
+                    break;
+                }
+                if (teller == 5) 
+                {
+                    // Stopt de huidige iteratie
+                    continue;
+                }
+                //Task.Delay(500).Wait();
+                Console.WriteLine($"Hello, World! {teller}");
+            }
+            //Console.WriteLine(teller);
+            Console.WriteLine("En verder....");
+        }
+    }
+}
